ac9737cb074b89e095ef0ac03428ef7a1cdcdc76
Nitingadhiya <vthink.nitin@gmail.com>
2021-04-20 15:19:23 +0530

    change server URL

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,10 +1,10 @@
 //const TestBasicURL = 'http://test.premiumitware.com/PSC_APP/AndroidService.svc';
 
 const BasicURL = {
-  path: 'http://appservices.premiumitware.com/AndroidService.svc',
+  path: 'http://appservices.premiumsales.in/AndroidService.svc',
   //socketURL: 'http://appservices.premiumitware.com:1315',
   //socketURL: 'http://10.0.2.2:3000',
-  socketURL: 'http://premiumitware.com:1315',
+  socketURL: 'http://premiumsales.in:1315',
   //socketURL: 'http://219.91.133.39:1315',
   //path: 'http://test.premiumitware.com/PSC_APP/AndroidService.svc', //staging
 };
